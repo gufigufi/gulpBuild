5f3604f67fccf82c5d8083e107fbe7936d1ce9ec
Кирилл Царёв <gufigufi.42@gmail.com>
2020-10-28 15:43:11 +0200

    gulp compil sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,54 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const gcmq = require('gulp-group-css-media-queries');
+const sassGlob = require('gulp-sass-glob');
+
+gulp.task('sass', function (callback) {
+    return gulp.src('./app/sass/main.sass')
+
+        .pipe(plumber({
+            errorHandler: notify.onError(function (err) {
+                return {
+                    title: 'Styles',
+                    sound: false,
+                    message: err.message
+                }
+            })
+        }))
+
+        .pipe(sourcemaps.init())
+        .pipe(sassGlob())
+        .pipe(sass({
+            indentType: 'tab',
+            indentWidth: 1,
+            outputStyle: 'expanded'
+        }))
+        .pipe(gcmq())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 4 version']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./app/css/'))
+    callback();
+});
 
 gulp.task('watch', function () {
+    // слежение за HTML и css и обновление браузера
     watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel(browserSync.reload));
+
+    // слежение за SASS и компиляция в CSS
+    watch('./app/sass/**/*.sass', gulp.parallel('sass'))
+
+    // в случае если возникает проблема что gulp перезапускает сервер быстрее чем файл успевает сохраниться
+    // watch('./app/sass/**/*.sass', function () {
+    //     setTimeout(gulp.parallel('sass'), 1000)
+    // })
 });
 
 // Задача для стратта сервера из папки app
@@ -15,4 +60,4 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('default', gulp.parallel('server','watch'))
+gulp.task('default', gulp.parallel('server','watch', 'sass'))
